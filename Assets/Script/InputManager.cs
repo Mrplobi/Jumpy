16bfb1c0bde09565fcdd84b7a9b1e19787d201d3
manzano alban <manzanoalban@gmail.com>
2018-10-02 15:54:39 +0200

    air speed & friction

--- Assets/Script/InputManager.cs
+++ Assets/Script/InputManager.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    public Physics player;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+       // Debug.Log(Input.GetAxis("Horizontal"));
+        player.Move(Input.GetAxis("Horizontal"));
+
+        if (Input.GetButtonDown("Jump"))
+        { player.Jump(); }
+       
 		
 	}
 }
--- Assets/Script/Physics.cs
+++ Assets/Script/Physics.cs
@@ -8,7 +8,17 @@ public class Physics : MonoBehaviour {
     public float mass;
     private Vector3 acceleration;
     public Vector3 velocity;
-
+    private bool isGrounded;
+    private bool isLocked = false;
+    public float groundSpeed;
+    public int numberJumpMax=2;
+    public int numberJumpCurrent=0;
+    public float groundedJumpImpulsion = 100;
+    public float airJumpImpulsion = 100;
+    public float airAcceleration;
+    public Vector2 airFriction;
+    public Vector2 groundFriction;
+    
     private void Start()
     {
         acceleration = new Vector3(0, 0, 0);
@@ -23,6 +33,12 @@ public class Physics : MonoBehaviour {
     private void GVelocity()
     {
         Vector3 new_velocity;
+        if (!isGrounded)
+        { velocity.x= Mathf.Abs(velocity.x) - airFriction.x > 0 ? velocity.x - Mathf.Sign(velocity.x) * airFriction.x:0;
+            velocity.y = Mathf.Abs(velocity.y) - airFriction.y > 0 ? velocity.y - Mathf.Sign(velocity.x)*airFriction.y : 0;
+
+        }
+          
         new_velocity = velocity + acceleration * Time.deltaTime;
         velocity = new_velocity;
     }
@@ -33,7 +49,32 @@ public class Physics : MonoBehaviour {
         new_pos = gameObject.transform.position + velocity * Time.deltaTime;
         gameObject.transform.position = new_pos;
     }
+    public void Move(float horizontal)
+    {
+        if (!isLocked && isGrounded)
+        {
+            velocity.x = horizontal * groundSpeed;
+        }
+        if (!isGrounded)
+        {
+            acceleration.x = horizontal * airAcceleration;
+        }
 
+    }
+    public void Jump()
+    {
+        Debug.Log("Bonjourno");
+        if(isGrounded && numberJumpCurrent<numberJumpMax)
+        { 
+        acceleration.y += groundedJumpImpulsion;
+            numberJumpCurrent++;
+        }
+        else if(!isGrounded && numberJumpCurrent < numberJumpMax)
+        {
+            acceleration.y += airJumpImpulsion;
+        numberJumpCurrent++;
+        }
+    }
     private void Update()
     {
         Gravity();
