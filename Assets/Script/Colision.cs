617158bb099fa0343ed17999a0d8d670c3d09cb8
Yann <26348667+Mrplobi@users.noreply.github.com>
2018-10-02 14:18:30 +0200

    gravité

---
+++ Assets/Script/Colision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colision : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("collision");
+        if (collision.GetComponent<Physics>() == null)
+        {
+            Debug.Log("no physics found");
+        }
+        else
+        {
+            Debug.Log("found Physics");
+            Vector3 newPos = new Vector3(0, 0, 0);
+            collision.transform.position = newPos;
+        }
+    }
+}
+
---
+++ Assets/Script/Physics.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physics : MonoBehaviour {
+
+    public float gravity;
+    public float mass;
+    private Vector3 acceleration;
+    public Vector3 velocity;
+
+    private void Start()
+    {
+        acceleration = new Vector3(0, 0, 0);
+        velocity = new Vector3(0, 0, 0);
+    }
+
+    private void Gravity()
+    {
+        acceleration.y = mass*gravity;
+    }
+
+    private void GVelocity()
+    {
+        Vector3 new_velocity;
+        new_velocity = velocity + acceleration * Time.deltaTime;
+        velocity = new_velocity;
+    }
+
+    private void Position()
+    {
+        Vector3 new_pos;
+        new_pos = gameObject.transform.position + velocity * Time.deltaTime;
+        gameObject.transform.position = new_pos;
+    }
+
+    private void Update()
+    {
+        Gravity();
+        GVelocity();
+        Position();
+    }
+}
