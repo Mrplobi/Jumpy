dfdf04ab082183eeb1af3d57c8da4c1b23f8d97b
Yann <26348667+Mrplobi@users.noreply.github.com>
2018-10-07 19:22:28 +0200

    wtf

--- Assets/Script/Colision.cs
+++ Assets/Script/Colision.cs
@@ -2,11 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Colision : MonoBehaviour
-{
+public class Colision : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (GetComponent<Collider2D>().bounds.min.y < other.bounds.max.y && GetComponent<Collider2D>().bounds.max.y > other.bounds.max.y)//On arrive par au dessus
+        if(GetComponent<Collider2D>().bounds.min.y< other.bounds.max.y && GetComponent<Collider2D>().bounds.max.y > other.bounds.max.y)//On arrive par au dessus
         {
             Physics physics = gameObject.GetComponent<Physics>();
 
@@ -14,20 +13,9 @@ public class Colision : MonoBehaviour
             physics.IsGrounded = true;
             physics.numberJumpCurrent = 0;
             physics.Velocity = new Vector3(physics.Velocity.x, 0, 0);
-            transform.position = new Vector3(transform.position.x, other.bounds.max.y + GetComponent<Collider2D>().bounds.size.y / 2);
+            transform.position = new Vector3(transform.position.x, other.bounds.max.y+ GetComponent<Collider2D>().bounds.size.y/2);
         }
-<<<<<<< HEAD
 
-=======
-        if (GetComponent<Collider2D>().bounds.min.y < other.bounds.min.y && GetComponent<Collider2D>().bounds.max.y > other.bounds.min.y)//On arrive par en dessous
-        {
-            Physics physics = gameObject.GetComponent<Physics>();
-
-            Debug.Log("BopBop DETECTED");
-            physics.Velocity = new Vector3(physics.Velocity.x, 0, 0);
-            transform.position = new Vector3(transform.position.x, other.bounds.min.y - GetComponent<Collider2D>().bounds.size.y / 2);
-        }
->>>>>>> 1cda4c755b71799f3515ae76d85472679cc704e5
     }
     public void DetectColision()
     {
@@ -35,43 +23,33 @@ public class Colision : MonoBehaviour
         //float size = gameObject.GetComponent<Collider2D>().Distance() * gameObject.transform.localScale.y;
         Vector3 velocity = physics.Velocity;
         //Vector2 direction = new Vector2(gameObject.GetComponent<Physics>().Velocity.x, gameObject.GetComponent<Physics>().Velocity.y);
-<<<<<<< HEAD
         Vector2 originD = new Vector2(gameObject.transform.position.x, gameObject.GetComponent<Collider2D>().bounds.min.y + 0.01f);
         Vector2 originU = new Vector2(gameObject.transform.position.x, gameObject.GetComponent<Collider2D>().bounds.max.y + 0.01f);
         Vector2 originR = new Vector2(gameObject.GetComponent<Collider2D>().bounds.max.x + 0.01f, gameObject.transform.position.y);
         Vector2 originL = new Vector2(gameObject.GetComponent<Collider2D>().bounds.min.x + 0.01f, gameObject.transform.position.y);
         RaycastHit2D hitD = Physics2D.Raycast(originD, -gameObject.transform.up, velocity.y * Time.deltaTime, LayerMask.GetMask("Environment"));
-=======
-        Vector2 origin = new Vector2(gameObject.transform.position.x, gameObject.GetComponent<Collider2D>().bounds.min.y + 0.01f);
-        Vector2 originU = new Vector2(gameObject.transform.position.x, gameObject.GetComponent<Collider2D>().bounds.max.y - 0.01f);
-        Vector2 originL = new Vector2(gameObject.transform.position.x, gameObject.GetComponent<Collider2D>().bounds.min.y + 0.01f);
-        Vector2 originR = new Vector2(gameObject.transform.position.x, gameObject.GetComponent<Collider2D>().bounds.min.y - 0.01f);
-        RaycastHit2D hitD = Physics2D.Raycast(origin, -gameObject.transform.up, velocity.y * Time.deltaTime, LayerMask.GetMask("Environment"));
->>>>>>> 1cda4c755b71799f3515ae76d85472679cc704e5
         RaycastHit2D hitU = Physics2D.Raycast(originU, gameObject.transform.up, velocity.y * Time.deltaTime, LayerMask.GetMask("Environment"));
-        RaycastHit2D hitR = Physics2D.Raycast(originL, gameObject.transform.right, velocity.x * Time.deltaTime, LayerMask.GetMask("Environment"));
-        RaycastHit2D hitL = Physics2D.Raycast(originR, -gameObject.transform.up, velocity.x * Time.deltaTime, LayerMask.GetMask("Environment"));
+        RaycastHit2D hitR = Physics2D.Raycast(originR, gameObject.transform.right, velocity.x * Time.deltaTime, LayerMask.GetMask("Environment"));
+        RaycastHit2D hitL = Physics2D.Raycast(originL, -gameObject.transform.right, velocity.x * Time.deltaTime, LayerMask.GetMask("Environment"));
         if (hitD)
         {
-            //  Debug.Log("collision");
+            Debug.Log("collisionDown");
             physics.IsGrounded = true;
-            /* Vector3 impact = new Vector3(hitD.point.x, hitD.point.y+
-                 gameObject.GetComponent<Collider2D>().bounds.size.y/2, 0);
-             gameObject.transform.position = impact;*/
-
+           /* Vector3 impact = new Vector3(hitD.point.x, hitD.point.y+
+                gameObject.GetComponent<Collider2D>().bounds.size.y/2, 0);
+            gameObject.transform.position = impact;*/
+            
             Vector3 newSpeed = new Vector3(physics.Velocity.x, 0, 0);
             physics.Velocity = newSpeed;
             physics.numberJumpCurrent = 0;
             transform.position = new Vector3(transform.position.x, hitD.collider.bounds.max.y + GetComponent<Collider2D>().bounds.size.y / 2);
             //check if jump buffered
-            InputButton action = GetComponent<InputManager>().SearchForFailedAction(physics.Jump, 10);
-            if (action != null)
+            InputButton action=GetComponent<InputManager>().SearchForFailedAction(physics.Jump, 10);
+            if (action!=null)
             {
                 action.Invoke();
-
             }
         }
-        
         else
         {
             physics.IsGrounded = false;
@@ -101,7 +79,7 @@ public class Colision : MonoBehaviour
 
     private void Update()
     {
-        // DetectColision();
+       // DetectColision();
     }
 }
 
--- Assets/Script/Physics.cs
+++ Assets/Script/Physics.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Physics : MonoBehaviour
-{
+public class Physics : MonoBehaviour {
 
     public float gravity;
     public float mass;
@@ -14,17 +13,13 @@ public class Physics : MonoBehaviour
     [SerializeField]
     private Vector3 velocity;
     private bool isGrounded;
-    private Coroutine coroutineDragging;
     private bool isLocked = false;
     public float groundSpeed;
-    public int numberJumpMax = 2;
-    public int numberJumpCurrent = 0;
+    public int numberJumpMax=2;
+    public int numberJumpCurrent=0;
     public float groundedJumpSpeed = 100;
     public float airJumpSpeed = 100;
     public float airAcceleration;
-    public float tetherSpeed;
-    public float tetherThreshHold = 3;
-    public float tetherDistance = 10;
     public Vector2 airFriction;
     public Vector2 groundFriction;
     public Vector3 Velocity
@@ -40,7 +35,7 @@ public class Physics : MonoBehaviour
         }
     }
 
-
+    
     public bool IsGrounded
     {
         get
@@ -65,23 +60,22 @@ public class Physics : MonoBehaviour
         acceleration.y = 0;
         if (!isGrounded)
         {
-            acceleration.y = mass * gravity + extraImpulsion.y;
+            acceleration.y = mass*gravity + extraImpulsion.y;
             extraImpulsion.y = 0;
         }
 
-
+        
     }
 
     private void GVelocity()
     {
         Vector3 new_velocity;
         if (!isGrounded)
-        {
-            velocity.x = Mathf.Abs(velocity.x) - airFriction.x * Mathf.Abs(velocity.x) > 0 ? velocity.x - Mathf.Sign(velocity.x) * airFriction.x * Mathf.Abs(velocity.x) : 0;
-            velocity.y = Mathf.Abs(velocity.y) - airFriction.y * Mathf.Abs(velocity.y) > 0 ? velocity.y - Mathf.Sign(velocity.y) * airFriction.y * Mathf.Abs(velocity.y) : 0;
+        { velocity.x= Mathf.Abs(velocity.x) - airFriction.x  * Mathf.Abs(velocity.x) > 0 ? velocity.x - Mathf.Sign(velocity.x) * airFriction.x * Mathf.Abs(velocity.x) : 0;
+            velocity.y = Mathf.Abs(velocity.y) - airFriction.y * Mathf.Abs(velocity.y) > 0 ? velocity.y - Mathf.Sign(velocity.y)*airFriction.y * Mathf.Abs(velocity.y) : 0;
 
         }
-
+          
         new_velocity = velocity + acceleration * Time.deltaTime;
         Velocity = new_velocity;
     }
@@ -104,65 +98,15 @@ public class Physics : MonoBehaviour
         }
         return true;
     }
-    public IEnumerator GetDragged(GameObject obj)
-    {
-
-
-        while ((obj.transform.position - transform.position).magnitude > tetherSpeed * Time.deltaTime * tetherThreshHold && tetherSpeed > 0)
-        {
-            Velocity = (obj.transform.position - transform.position).normalized * tetherSpeed;
-            yield return null;
-        }
-        coroutineDragging = null;
-    }
-    public bool Tether()
-    {
-        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, tetherDistance, LayerMask.GetMask("Tether"));
-        float closestDistance = tetherDistance;
-        Collider2D closestHit = null;
-        Debug.Log("Size tether hits" + hits.Length);
-        foreach (Collider2D hit in hits)//get closest and get dragged to it
-        {
-            Debug.Log("Tether distance" + (hit.transform.position - transform.position).magnitude);
-            Debug.Log(hit.gameObject);
-            if (hit.GetComponent<TetherPoint>() && (hit.transform.position - transform.position).magnitude < closestDistance)
-            {
-                closestHit = hit;
-                closestDistance = (hit.transform.position - transform.position).magnitude;
-            }
-
-        }
-        Debug.Log(closestHit);
-        if (!closestHit) //Must check if another one though
-        {
-            Debug.Log("Tether failed");
-            return false;
-        }
-        else
-        {
-            Debug.Log("START tether");
-            coroutineDragging = StartCoroutine(GetDragged(closestHit.gameObject));
-            return true;
-        }
-
-    }
     public bool Jump()
-<<<<<<< HEAD
     {               
         if (isGrounded && numberJumpCurrent<numberJumpMax)
         { 
-=======
-    {
-
-
-        if (isGrounded && numberJumpCurrent < numberJumpMax)
-        {
->>>>>>> 1cda4c755b71799f3515ae76d85472679cc704e5
             velocity.y = groundedJumpSpeed;
             isGrounded = false;
             numberJumpCurrent++;
         }
-        else if (!isGrounded && numberJumpCurrent < numberJumpMax)
+        else if(!isGrounded && numberJumpCurrent < numberJumpMax)
         {
             velocity.y = airJumpSpeed;
             numberJumpCurrent++;
@@ -171,18 +115,11 @@ public class Physics : MonoBehaviour
         return false;
     }
     private void Update()
-<<<<<<< HEAD
     { 
-=======
-    {
->>>>>>> 1cda4c755b71799f3515ae76d85472679cc704e5
         Gravity();
         GVelocity();
         Position();
         GetComponent<Colision>().DetectColision();
-<<<<<<< HEAD
        
-=======
->>>>>>> 1cda4c755b71799f3515ae76d85472679cc704e5
     }
 }
