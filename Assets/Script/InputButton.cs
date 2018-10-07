e8e8bab06c01ba1721a6b3c102171d546d95b17b
manzano alban <manzanoalban@gmail.com>
2018-10-07 15:43:15 +0200

    modulo negative but still2

---
+++ Assets/Script/InputButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputButton {
+    public delegate bool InputAction();
+    public InputAction actualAction;
+    public bool actionSucceeded;
+    // Use this for initialization
+    public InputButton(InputAction inputAction)
+    {
+        this.actualAction = inputAction;
+    }
+    public void Invoke()
+    {
+        actionSucceeded=actualAction.Invoke();
+    }
+}
