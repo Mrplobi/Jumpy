975b88ae0647af27d5415d329fc57f7a773a342f
Yann <26348667+Mrplobi@users.noreply.github.com>
2018-10-15 09:07:54 +0200

    map encore

---
+++ Assets/Script/Respawn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+    public GameObject respawnPoint;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.transform.position = respawnPoint.transform.position;
+    }
+}
